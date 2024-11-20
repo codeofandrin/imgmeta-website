bdc81572a7090583fa626b610b8c57692e67f5c2
Andrin <65789180+codeofandrin@users.noreply.github.com>
2024-11-20 22:24:38 +0100

    Add work in progress status

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./styles/App.css"
-
 function App() {
-  return <></>
+  return (
+    <div className="flex flex-col h-full w-full items-center justify-center">
+      <h1 className="text-7xl font-black">Work in progress... 🔨</h1>
+      <p className="mt-14 text-zinc-500">
+        See how it's built on{" "}
+        <a className="text-zinc-400 hover:underline" href="https://github.com/codeofandrin/imgmeta-website" target="_blank">
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  )
 }
 
 export default App
